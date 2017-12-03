f7169f0a95f1ac6e8ba1137d12f74de93082b49d
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-12-03 00:46:04 -0500

    set up db connection and express connection

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+
+var port = 3000;
+
+var app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//import routes
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+app.listen(port);
+
+console.log('listening on port ' + port)

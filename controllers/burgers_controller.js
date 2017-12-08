af26264cf52dccc80165dfda2a7398f86272fc96
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-12-07 23:17:33 -0500

    working on post route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,26 +11,23 @@ router.get('/', function(req, res) {
 	});
 });
 
-router.post('/api/burgers', function(req, res) {
+router.post('/', function(req, res) {
 	burger.insertOne([
 		'burger_name', 'devoured'
 		], [
-			req.body.burger_name, req.body.devoured
+		req.body.burger_name, req.body.devoured
 		], function() {
 			res.redirect("/");
 	});
 });
 
-router.put('/api/burgers/:id', function(req, res) {
+router.put('/:id', function(req, res) {
 	var condition = 'id = ' + req.params.id;
-	burger.update({
+	
+	burger.updateOne({
 		devoured: req.body.devoured
-	}, condition, function(result) {
-		if (result.changedRows == 0) {
-			return res.status(404).end();
-		} else {
-			res.status(200).end();
-		}
+	}, condition, function() {
+		res.redirect('/')
 	});
 });
 

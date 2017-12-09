abcd7c732a3c32c750dc70329f77e680c8c197fe
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-12-09 12:20:40 -0500

    server

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-insert into burgers (burger_name) values ("Vegetative State");
-insert into burgers (burger_name) values ("I Can't Believe It's Not Burger");
-insert into burgers (burger_name, devoured) values ("Mother of Daikons", true);
+insert into burgers (burger_name) values ("veggie burger");
+insert into burgers (burger_name) values ("cheeseburger");
+insert into burgers (burger_name) values ("bacon cheeseburger");
